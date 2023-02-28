2bce79f63dcfbc6fc4f8f1e9f588d02335e0e9fc
Matryoshka <95856798+lixiaowei11111@users.noreply.github.com>
2023-02-28 20:01:47 +0800

    data router

--- webpack5_react18_ts_template/src/App.tsx
+++ webpack5_react18_ts_template/src/App.tsx
@@ -1,15 +1,20 @@
 // App.tsx
 
 import React from 'react'
+import { useParams } from 'react-router-dom'
+
 import { AppContext } from './components/AppContext'
 
 const App = () => {
-  return (
-    <div>
-      <span>12312312331</span>
-      <AppContext></AppContext>
-    </div>
-  )
+	const params = useParams()
+	console.log(params, 'useParams()')
+
+	return (
+		<div>
+			<span>12312312331</span>
+			<AppContext></AppContext>
+		</div>
+	)
 }
 
 export default App
--- webpack5_react18_ts_template/src/index.tsx
+++ webpack5_react18_ts_template/src/index.tsx
@@ -9,7 +9,61 @@
 // react18 之后
 import React from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App'
-console.log(process.env, 'process.env') // 通过webpack的DefinePlugin接受替换的全局变量 aaa
+import { Provider } from 'react-redux'
+import {
+	BrowserRouter,
+	createBrowserRouter,
+	RouterProvider,
+	Route,
+	createRoutesFromElements,
+} from 'react-router-dom'
+import ErrorBoundary from '@/components/ErrorBoundary'
 
-createRoot(document.getElementById('root')).render(<App />)
+import store from '@/redux'
+import App from './App'
+
+console.log(process.env, 'process.env') // 通过webpack的DefinePlugin接受替换的全局变量
+
+// //1. 路由的常规创建方法
+// createRoot(document.getElementById('root')).render(
+// 	<ErrorBoundary>
+// 		<Provider store={store}>
+// 			<BrowserRouter>
+// 				<App />
+// 			</BrowserRouter>
+// 		</Provider>
+// 	</ErrorBoundary>,
+// )
+
+// 2. 使用 createBrowserRouter 创建
+const router = createBrowserRouter([
+	{
+		path: '/home',
+		element: <App />,
+		loader: null,
+		action: null,
+		errorElement: <ErrorBoundary />,
+		children: null,
+	},
+])
+// 或者 使用 createRoutesFromElements
+// const router = createBrowserRouter(
+// 	createRoutesFromElements(
+// 		<Route
+// 			path="home"
+// 			element={<App />}
+// 			loader={null}
+// 			action={null}
+// 			errorElement={<ErrorBoundary />}
+// 			children={null}
+// 		/>,
+// 	),
+// )
+
+createRoot(document.getElementById('root')).render(
+	<ErrorBoundary>
+		<Provider store={store}>
+			<RouterProvider router={router}></RouterProvider>
+		</Provider>
+	</ErrorBoundary>,
+)
